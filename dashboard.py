d64d29b037452fe341f150df1ce21592d6c5eee0
Austin Caudill <31804903+AustinCaudill@users.noreply.github.com>
2021-11-11 06:54:46 -0600

    Updated

--- dashboard.py
+++ dashboard.py
@@ -17,7 +17,7 @@ LOGO = ""
 
 navbar = dbc.NavbarSimple(
     children=[
-        dbc.NavItem(dbc.NavLink("The Petro Guy", href="https://www.thepetroguy.com")),
+        dbc.NavItem(dbc.NavLink("The Petro Guy", href="https://www.thepetroguy.com", target="_new")),
         dbc.DropdownMenu(
             children=[
                 dbc.DropdownMenuItem("More pages", header=True),
@@ -26,8 +26,7 @@ navbar = dbc.NavbarSimple(
             nav=True,
             in_navbar=True,
             label="Explore",
-        ),
-    ],
+        )],
     brand="Decline Curve Generator",
     brand_href="#",
     color="primary",
@@ -35,40 +34,99 @@ navbar = dbc.NavbarSimple(
     class_name="mb-4",
 )
 
-inputs = dbc.Card(
-    [
-        dcc.Input(
-        id="q_init",
-        placeholder='Initial Rate (STB/day)',
-        type='number',
-        value='1000',
-        className="my-4 p-4"
-        ),
-        dcc.Input(
-        id="q_next",
-        placeholder='Next Rate (STB/day)',
-        type='number',
-        value='900',
-        className="my-4 p-4"
-        ),
-        dcc.Input(
-        id="t_months",
-        placeholder='Time (Months)',
-        type='number',
-        value='1',
-        className="my-4 p-4"
-        ),
-        dcc.Input(
-        id="t_tot",
-        placeholder='Project Life',
-        type='number',
-        value='120',
-        className="my-4 p-4"
-        ),
-    ]
+Body = dcc.RadioItems(
+    id="decline_choice",
+    options=[
+        {'label': 'Exponential', 'value': 'EXP'},
+        {'label': 'Hyperbolic', 'value': 'HYP'},
+        {'label': 'Harmonic', 'value': 'HAR'}
+    ],
+    value='EXP',
+    labelStyle={'display': 'flex'}
 )
 
-card = dbc.Card([dbc.CardHeader("Header"), dbc.CardBody("Body", style={"height": 250})])
+footer = html.Div(
+        dbc.Row(dbc.Card(("© 2021 Austin Caudill"), className="text-center"))
+)
+
+inputs = dbc.CardGroup(
+        [
+        dbc.Card(
+            dbc.CardBody(
+                [
+                    html.H5("Initial Rate (qi)", className="card-title"),
+                    html.P(
+                        "Input the initial rate, qi, in STB/day.",
+                        className="card-text",
+                    ),
+                    dcc.Input(
+                    id="q_init",
+                    placeholder='Initial Rate (STB/day)',
+                    type='number',
+                    value='1000',
+                    className="me-auto"
+                    )
+                ]
+            )
+        ),
+        dbc.Card(
+            dbc.CardBody(
+                [
+                    html.H5("Production Rate (qt)", className="card-title"),
+                    html.P(
+                        "Input the production rate, qt, at time (t) in STB/day.",
+                        className="card-text",
+                    ),
+                    dcc.Input(
+                    id="q_next",
+                    placeholder='Current Rate (STB/day)',
+                    type='number',
+                    value='900',
+                    className="me-auto"
+                    )
+                ]
+            )
+        ),
+        dbc.Card(
+            dbc.CardBody(
+                [
+                    html.H5("Cum Time (t)", className="card-title"),
+                    html.P(
+                        "Input the cumulative time, in months, since start of production.",
+                        className="card-text",
+                    ),
+                    dcc.Input(
+                    id="t_months",
+                    placeholder='Time (Months)',
+                    type='number',
+                    value='1',
+                    className="me-auto"
+                    )
+                ]
+            )
+        ),
+        dbc.Card(
+            dbc.CardBody(
+                [
+                    html.H5("Time to Generate", className="card-title"),
+                    html.P(
+                        "Input the total amount of months to plot.",
+                        className="card-text",
+                    ),
+                    dcc.Input(
+                    id="t_tot",
+                    placeholder='Project Life',
+                    type='number',
+                    value='120',
+                    className="me-auto"
+                    )
+                ]
+            )
+        ),
+        ]
+)
+
+sidebar = dbc.Card([dbc.CardHeader("Types of Decline"), dbc.CardBody(Body, style={"height": 250})])
 
 graph_card = dbc.Card(
     dbc.CardBody([dcc.Graph(id="fig", style={"height": 400})]), className="my-4"
@@ -79,27 +137,46 @@ app.layout = dbc.Container(
         navbar,
         dbc.Row(
             [
-                dbc.Col([card, inputs], width=3),
+                dbc.Col([sidebar], width=3),
                 dbc.Col(
                     [
+                        dbc.Row(inputs),
                         graph_card,
                     ],
                     width=8,
                 ),
             ]
         ),
+        footer,
     ],
     fluid=True,
 )
+
+
+
+    
+@app.callback(
+    Output('b', 'value'),
+    Input('decline_choice', 'value'))
+def select_decline(decline_choice):
+    if decline_choice == 'EXP':
+        b = 0
+    elif decline_choice == 'HYP':
+        b = 1
+    elif decline_choice == 'HAR':
+        b = 2    
+    else:
+        b = 0
+    return b
 
 @app.callback(
     Output('fig', 'figure'),
     Input('q_init', 'value'),
     Input('q_next', 'value'),
     Input('t_months', 'value'),
-    Input('t_tot', 'value'))
-
-def update_fig(q_init,q_next,t_months, t_tot):
+    Input('t_tot', 'value'),
+    Input('decline_choice', 'value'))
+def update_fig(q_init,q_next,t_months, t_tot, decline_choice):
     # Input Data
     t = np.arange(0.1,int(t_tot),0.5) # 120 months by 1/2 month
     t_0m = 0 # month
@@ -107,11 +184,9 @@ def update_fig(q_init,q_next,t_months, t_tot):
     q_next = int(q_next)
     t_months = int(t_months)
 
-    # Exponential Decline
-
-    b = np.log(q_init/q_next)/(t_months - t_0m)
-    q = q_init * np.exp(-b * t) # Forecast
-    Np = (q_init - q) * 365/(b * 12)
+    D = np.log(q_init/q_next)/(t_months - t_0m) # Decline Rate
+    q = q_init * np.exp(-D * t) # Forecast
+    Np = (q_init - q) * 365/(D * 12) # Volume Produced
 
     # Plot
     fig = make_subplots(specs=[[{"secondary_y": True}]])
@@ -119,18 +194,21 @@ def update_fig(q_init,q_next,t_months, t_tot):
     fig.add_trace(go.Scatter(x=t, y=(Np/1000000), name='Cum Production (Np)', line=dict(color='green', width=4, dash='dash')), secondary_y=True)
 
     # Set x-axis title
-    fig.update_xaxes(title_text="Time in Months")
+    fig.update_xaxes(title_text="Cumulative Time (Months)")
 
     # Set y-axes titles
     fig.update_yaxes(title_text="Rate (STB/Day)", secondary_y=False)
     fig.update_yaxes(title_text="Cum Production (MMSTB)", secondary_y=True)
 
     fig.update_layout(
-        title={'text': "Exponential", 'xanchor': "center"},
-        legend={
-        'yanchor': "top",
-        'xanchor': "right",
-        }
+        title={'text': "Decline Curves"},
+        legend=dict(
+            orientation="h",
+            yanchor="bottom",
+            y=1.02,
+            xanchor="right",
+            x=1
+        )
         )
     return fig
 
